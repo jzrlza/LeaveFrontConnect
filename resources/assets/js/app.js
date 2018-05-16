5a01355318ad046599b00f92bb4337391bdd1baf
jzrlza <parinvut12345@gmail.com>
2018-05-16 23:43:02 +0700

    Front-end now has Passport

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -121,7 +121,20 @@ Vue.component('sub-accept-tasks', require('./components/SubAcceptTasks.vue'));
 Vue.component('sub-accept-requests', require('./components/SubAcceptRequests.vue'));
 Vue.component('sub-check', require('./components/SubCheck.vue'));
 
+Vue.component(
+    'passport-clients',
+    require('./components/passport/Clients.vue')
+);
 
+Vue.component(
+    'passport-authorized-clients',
+    require('./components/passport/AuthorizedClients.vue')
+);
+
+Vue.component(
+    'passport-personal-access-tokens',
+    require('./components/passport/PersonalAccessTokens.vue')
+);
 
 const app = new Vue({
 	router,
