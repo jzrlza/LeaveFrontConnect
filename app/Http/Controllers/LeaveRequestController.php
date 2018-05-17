57ba70c19e0ae228b2ec35ece5a6feb46a0574ea
kwanwantku <kwanwan.t@ku.th>
2018-05-17 21:35:42 +0700

    delete AssignedTask and modify to combine with Task add create Task and delete Task

--- app/Http/Controllers/LeaveRequestController.php
+++ app/Http/Controllers/LeaveRequestController.php
@@ -9,7 +9,7 @@
 namespace App\Http\Controllers;
 
 
-class LeaveRequestController
+class LeaveRequestController extends Controller
 {
 
 }
--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -9,7 +9,26 @@
 namespace App\Http\Controllers;
 
 
-class TaskController
+class TaskController extends Controller
 {
+    public function Create(Request $request)
+    {
+        $task = Task::create([
+            'title' => $request->get('task_name'),
+            'detail' => $request->get('detail'),
+            'deadline' => $request->get('detail'),
+            'user_id' => $request->user()->id,
+            'done' => $request->get('done'),
+            'priority' => $request->get('priority'),
+            'expected_date' => $request->get('expected_date')
+        ]);
+        return $task;
+    }
+
+    public function delete(Task $task)
+    {
+        $task->delete();
+        return response()->json(null, 204);
+    }
 
 }
